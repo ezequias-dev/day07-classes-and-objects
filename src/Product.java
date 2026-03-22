198395772f8d00aac2cb62f7bfdafc4f2e663866
ezequias-dev <ezequiasd30@gmail.com>
2026-03-22 19:24:00 -0300

    feat: adiciona implementacao guiada exemplo 1

---
+++ src/Product.java
@@ -0,0 +1,11 @@
+public class Product {
+    String name;
+    double price;
+    int stock;
+
+    void showDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Price: " + price);
+        System.out.println("Stock: " + stock);
+    }
+}
